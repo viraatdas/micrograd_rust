75cda9d3f7d39384416ac6793557c1601cc60fe1
Viraat <viraat.laldas@gmail.com>
2023-10-19 13:50:52 -0700

    add children

--- examples/demo.rs
+++ examples/demo.rs
@@ -5,7 +5,8 @@ use micrograd::engine::Value; // or whatever the path is to the Value struct
 fn main() {
     let v1 = Value::new(1.0);
     let v2 = Value::new(2.0);
-    let v3 = v1 + v2;
+
+    let v3 = v1.clone() + v2.clone();
     println!("Sum is: {}", v3.data);
 
     let v4 = v1 * v2;
--- src/engine.rs
+++ src/engine.rs
@@ -1,17 +1,32 @@
 use std::ops::{Add, Mul};
 
-#[derive(Clone, Copy)]
+#[derive(Clone)]
 pub struct Value {
     pub data: f64,
+    pub children: Vec<Value>,
+    pub op: String, // operation that produced this node
+}
+
+impl Value {
+    pub fn new(data: f64) -> Self {
+        Self {
+            data,
+            op: "".to_string(),
+            children: Vec::new(),
+        }
+    }
 }
 
 impl Add for Value {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
-        Self {
-            data: self.data + other.data,
-        }
+        let new_data = self.data + other.data;
+        let mut new_value = Value::new(new_data);
+        new_value.op = "+".to_string();
+        new_value.children.push(self);
+        new_value.children.push(other);
+        new_value
     }
 }
 
@@ -19,14 +34,11 @@ impl Mul for Value {
     type Output = Self;
 
     fn mul(self, other: Self) -> Self {
-        Self {
-            data: self.data * other.data,
-        }
-    }
-}
-
-impl Value {
-    pub fn new(data: f64) -> Self {
-        Self { data }
+        let new_data = self.data * other.data;
+        let mut new_value = Value::new(new_data);
+        new_value.op = "*".to_string();
+        new_value.children.push(self);
+        new_value.children.push(other);
+        new_value
     }
 }
